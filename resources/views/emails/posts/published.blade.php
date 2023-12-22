63eb69b55bc89c0db24450f7a173c7c45c9940e2
Mehdi <mdaalvand@gmail.com>
2023-12-22 16:12:47 +0330

    fix email style

--- resources/views/emails/posts/published.blade.php
+++ resources/views/emails/posts/published.blade.php
@@ -1,18 +1,18 @@
 <x-mail::message>
-    # New Post Published
+# New Post Published
 
-    A new post has been published on the website you're subscribed to.
+A new post has been published on the website you're subscribed to.
 
-    ## Post Title
-    {{ $post->title }}
+## Post Title
+{{ $post->title }}
 
-    ## Post Description
-    {{ $post->description }}
+## Post Description
+{{ $post->description }}
 
-    <x-mail::button :url="$post->url">
-        Read More
-    </x-mail::button>
+<x-mail::button :url="$post->url">
+    Read More
+</x-mail::button>
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 </x-mail::message>
